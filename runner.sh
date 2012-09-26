7cd8803962697013977421edc0c3b7331705d7be
fmaia <fmaia@di.uminho.pt>
2012-09-26 18:41:27 +0100

    runner changed

--- runner.sh
+++ runner.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-PYTHONPATH=./src:./src/config:./src/external:./libs/py4j:./libs/paramiko python src/main.py > sidelog.log &
+PYTHONPATH=./src:./src/config:./src/external:./libs/py4j:./libs/paramiko python src/main.py
+
+#> sidelog.log &
