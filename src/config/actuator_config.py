13abdcd0e56c99d837cb94276b7f43ddfdf9c1f8
francisco <fmcruz@di.uminho.pt>
2012-10-07 11:50:33 +0100

    updated actuator_config

--- src/config/actuator_config.py
+++ src/config/actuator_config.py
@@ -1,8 +1,8 @@
 __author__ = 'fmaia'
 
 master = 'master'
-template= '/home/gsd/met/MeTGlue/python/tmp/hbase-site_template.xml'
-target= '/home/gsd/met/MeTGlue/python/tmp/hbase-site.xml'
+template= '/home/gsd/MeT2/tmp/hbase-site_template.xml'
+target= '/home/gsd/MeT2/tmp/hbase-site.xml'
 whereto= '/opt/hbase-0.92.0-cdh4b1-rmv/conf/'
 username = 'root'
 password = '123456'
